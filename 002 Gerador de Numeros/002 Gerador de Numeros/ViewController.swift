7b24c93c75826b0f0069bcaef43836a957645881
vloss <viniciusvianaloss@gmail.com>
2023-03-14 16:33:41 -0300

    Finalização de App.

--- 002 Gerador de Numeros/002 Gerador de Numeros/ViewController.swift
+++ 002 Gerador de Numeros/002 Gerador de Numeros/ViewController.swift
@@ -8,12 +8,23 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var lbNumber: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func getNumberRandom(_ sender: UIButton) {
+        //sender.setImage(UIImage(named: "botaogerar-pressionado"), for: .normal)
+        
+        lbNumber.text = "\(arc4random_uniform(10))"
+        
+//        Timer.scheduledTimer(withTimeInterval: 0.5, repeats: false) { (timer) in
+//            sender.setImage(UIImage(named: "botaogerar"), for: .normal)
+//        }
+        
+    }
 }
 
