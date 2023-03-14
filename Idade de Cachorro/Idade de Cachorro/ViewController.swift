3a8423b29874c7729ec004c7c641cbb15420f5ec
vloss <viniciusvianaloss@gmail.com>
2023-03-14 01:59:14 -0300

    Finalização de App idade de cachorro.

--- Idade de Cachorro/Idade de Cachorro/ViewController.swift
+++ Idade de Cachorro/Idade de Cachorro/ViewController.swift
@@ -9,11 +9,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var lbLegend: UILabel!
+    @IBOutlet weak var tfAgeDog: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func getAgeDogInAgeHuman(_ sender: Any) {
+        lbLegend.text = "A idade do cachorro em anos humanos é: \(Int(tfAgeDog.text!)! * 7)"
+    }
+    
 }
 
