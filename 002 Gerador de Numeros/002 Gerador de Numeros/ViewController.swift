d28df06e8b192e092682c715eeefddeb773f1039
vloss <viniciusvianaloss@gmail.com>
2023-03-14 15:22:02 -0300

    Criando projeto para App 02 do curso.

---
+++ 002 Gerador de Numeros/002 Gerador de Numeros/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  002 Gerador de Numeros
+//
+//  Created by Vinicius Loss on 14/03/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
