fb84d69c65dddd846401729c6cd6dbbc4b95be28
vloss <viniciusvianaloss@gmail.com>
2023-03-14 23:18:21 -0300

    Finalização de App Frase do Dia.

--- 003 Frases do Dia/003 Frases do Dia/ViewController.swift
+++ 003 Frases do Dia/003 Frases do Dia/ViewController.swift
@@ -8,12 +8,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var quotes: [String] = []
+    
+    @IBOutlet weak var lbQuoteDay: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        quotes.append("Ter sucesso é falhar repetidamente, mas sem perder o entusiasmo!!!")
+        quotes.append("Às vezes, a vida vai te acertar um tijolo na cabeça. Não perca a fé.")
+        quotes.append("A vida não é sobre ter, é sobre dar e ser.")
+        quotes.append("Uma ideia é um feito de associação.")
+        quotes.append("Nós nos tornamos aquilo que pensamos.")
     }
 
-
+    @IBAction func newQuoteDay(_ sender: UIButton) {
+        let index = Int(arc4random_uniform(UInt32(quotes.count)))
+        lbQuoteDay.text = quotes[index]
+    }
 }
 
