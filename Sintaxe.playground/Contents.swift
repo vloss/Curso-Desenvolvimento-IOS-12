01b91c45c79b333dd848fd1b0a896665767ddb15
vloss <viniciusvianaloss@gmail.com>
2023-03-14 23:30:02 -0300

    Playground de Sintaxe.

---
+++ Sintaxe.playground/Contents.swift
@@ -0,0 +1,21 @@
+import UIKit
+
+// Arrays
+var lista_compras: [String] = []
+lista_compras.append("Refri")
+lista_compras.append("Queijo")
+lista_compras.append("Alface")
+lista_compras.append("Alface") // Itens iguais se repetem
+print(lista_compras)
+
+// Sets
+var lista = Set<String>()
+lista.insert("Suco")
+lista.insert("Pão")
+lista.insert("Tomate")
+lista.insert("Tomate") // Não se repente os itens iguais
+print(lista)
+
+
+// Dicionarios
+
