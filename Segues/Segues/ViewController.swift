3a8edb41dca07b48d46fc3ee05ab0f97e116461f
vloss <viniciusvianaloss@gmail.com>
2023-03-16 18:49:33 -0300

    Revisão de Segues

---
+++ Segues/Segues/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Segues
+//
+//  Created by Vinicius Loss on 16/03/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
