172470e7fae9cb23904dff6eb73e15e30c35b506
vloss <viniciusvianaloss@gmail.com>
2023-03-16 11:03:10 -0300

    Finalização de App Alcool ou Gasolina.

--- 004 Gasolina ou Alcool/004 Gasolina ou Alcool/ViewController.swift
+++ 004 Gasolina ou Alcool/004 Gasolina ou Alcool/ViewController.swift
@@ -8,12 +8,41 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let formatter = NumberFormatter()
 
+    @IBOutlet weak var lbResult: UILabel!
+    @IBOutlet weak var tfPriceAlcohol: UITextField!
+    @IBOutlet weak var tfPriceGasoline: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    func calculateBestPrice(alcohol: String, gasoline: String){
 
+        let formatterAlcohol    = formatter.number(from: alcohol)!.doubleValue
+        let formatterGasoline   = formatter.number(from: gasoline)!.doubleValue
+            
+        if (formatterAlcohol / formatterGasoline) >= 0.7 {
+            lbResult.text = "Melhor utilizar gasolina!"
+        } else {
+            lbResult.text = "Melhor utilizar álcool!"
+        }
+        
+    }
 
+    @IBAction func calculate(_ sender: UIButton) {
+        //lbResult.text = "\(formatter.number(from: tfPriceAlcohol.text ?? "0,00")!.doubleValue)"
+        
+        if let priceAlcohol = tfPriceAlcohol.text, let priceGasoline = tfPriceGasoline.text, !priceAlcohol.isEmpty, !priceGasoline.isEmpty {
+
+            calculateBestPrice(alcohol: priceAlcohol, gasoline: priceGasoline)
+
+        } else {
+            lbResult.text = "Informe os valores de gasolina e álcool!"
+        }
+    }
+    
 }
-
