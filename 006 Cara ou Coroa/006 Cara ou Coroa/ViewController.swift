2c47cf75b6e1857afebe6dbd4f62ab8ffc847afe
vloss <viniciusvianaloss@gmail.com>
2023-03-17 14:45:35 -0300

    Inicio de Criação do App Cara ou Coroa.

---
+++ 006 Cara ou Coroa/006 Cara ou Coroa/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  006 Cara ou Coroa
+//
+//  Created by Vinicius Loss on 17/03/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
